fd08c8f6adb88ebc01e9904d552f829c81e83e99
kouonang-hermann <kouonanghermann08@gmail.com>
2026-06-15 06:58:13 +0100

    mobile2

--- components/layout/layout-wrapper.tsx
+++ components/layout/layout-wrapper.tsx
@@ -4,6 +4,7 @@ import { useStore } from "@/stores/useStore"
 import Navbar from "./navbar"
 import { TooltipProvider } from "@/components/ui/tooltip"
 import { SignatureProvider } from "@/hooks/use-ensure-signature"
+import MobileInjectorWrapper from "@/components/mobile/mobile-injector-wrapper"
 
 export default function LayoutWrapper({ children }: { children: React.ReactNode }) {
   const { isAuthenticated } = useStore()
@@ -14,6 +15,7 @@ export default function LayoutWrapper({ children }: { children: React.ReactNode 
   return (
     <TooltipProvider delayDuration={200}>
       <SignatureProvider>
+        <MobileInjectorWrapper />
         {showNavbar && <Navbar />}
         {children}
       </SignatureProvider>
